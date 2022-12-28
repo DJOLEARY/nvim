3f010f1bd2ad8e4d579262638074ad976a6019e1
D.J. O'Leary <djoleary39@ymail.com>
2022-12-28 18:17:34 +0000

    feat: Add neovide configuration

--- lua/dijitol/init.lua
+++ lua/dijitol/init.lua
@@ -1,3 +1,4 @@
 require("dijitol.remap")
 require("dijitol.set")
 require("dijitol.highlightyank")
+require("dijitol.neovide")
---
+++ lua/dijitol/neovide.lua
@@ -0,0 +1,11 @@
+-- [[ Neovide ]]
+-- See https://neovide.dev/configuration.html
+if vim.g.neovide then
+
+  -- Set typeface and size
+  vim.opt.guifont = 'Fira Code:h14'
+
+  -- Turn on Railgun trail
+  vim.g.neovide_cursor_vfx_mode = 'railgun'
+
+end
