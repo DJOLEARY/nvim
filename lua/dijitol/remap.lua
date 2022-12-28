993f72e67aa50351b90dabf704d65e2e1f389fe5
D.J. O'Leary <djoleary39@ymail.com>
2022-12-28 21:09:28 +0000

    feat: Changed leader + f to command

--- lua/dijitol/remap.lua
+++ lua/dijitol/remap.lua
@@ -20,8 +20,10 @@ vim.keymap.set('n', '<leader>ex', vim.cmd.Ex)
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
 
--- Keep cursor centered
+-- Keep cursor at beginning of line when joining
 vim.keymap.set('n', 'J', 'mzJ`z')
+
+-- Keep cursor centered
 vim.keymap.set('n', '<C-d>', '<C-d>zz')
 vim.keymap.set('n', '<C-u>', '<C-u>zz')
 vim.keymap.set('n', 'n', 'nzzzv')
@@ -31,14 +33,14 @@ vim.keymap.set('n', 'N', 'Nzzzv')
 vim.keymap.set('x', '<leader>p', '\"_dP')
 
 -- Copy to system's clipboard
-vim.keymap.set({'n', 'v'}, '<leader>y', '\"+y')
+vim.keymap.set({ 'n', 'v' }, '<leader>y', '\"+y')
 vim.keymap.set('n', '<leader>Y', '\"+Y')
 
 -- Delete without saving deleted characters to register
-vim.keymap.set({'n', 'v'}, '<leader>d', '\"_d')
+vim.keymap.set({ 'n', 'v' }, '<leader>d', '\"_d')
 
 -- Call LSP format
-vim.keymap.set('n', '<leader>f', ':Format')
+vim.keymap.set('n', '<leader>f', vim.cmd.Format)
 
 -- Quickfix list
 vim.keymap.set('n', '<C-k>', '<cmd>cnext<CR>zz')
