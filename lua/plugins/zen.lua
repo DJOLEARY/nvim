0e3fdf6c4e65f91080fdb10a011c23afacddbbe9
DJ O'Leary <dijitol@proton.me>
2025-02-16 15:46:34 +0100

    feat: Replace noneckpain with zenmode

---
+++ lua/plugins/zen.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "folke/zen-mode.nvim",
+    keys = {
+      {
+        "<leader>z",
+        function() require("zen-mode").toggle() end,
+        desc = "Toggle [Z]enMode",
+      }
+    },
+    opts = {
+      plugins = {
+        options = {
+          laststatus = 3,
+        },
+      },
+    },
+  },
+}
