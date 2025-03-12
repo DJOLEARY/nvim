e52bc765bff7f538b6ca7bcb29936ef562153513
DJ O'Leary <dj.o.leary@cardmarket.com>
2025-03-12 16:55:40 +0100

    feat: remove automatic update check

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -32,7 +32,7 @@ require("lazy").setup({
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { colorscheme } },
   -- automatically check for plugin updates
-  checker = { enabled = true },
+  checker = { enabled = false },
 })
 
 -- Set colorscheme after all plugins have been installed
